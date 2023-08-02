412c3cbef4ac045630834af3469c9a0c4c7b99ea
Mr.X <isuruumayanga035@gmail.com>
2023-08-02 21:42:39 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,11 @@ setup(
     version="1.0",
     author="MRX",
     description="YouTube Audio Downloader",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     author_email = 'trymrx@pm.me',
     url = 'https://github.com/isuruwa/YT2AUDIO',
-    download_url = '',
+    download_url = 'https://github.com/isuruwa/YT2AUDIO/blob/main/dist/yt2audio-1.0.tar.gz',
     packages=find_packages(),
     entry_points={
         "console_scripts": [
